8551c6d9071fba04bb0bdaa077fd71ebaaa5f1b6
shobeedev <moha.saoud22@gmail.com>
2026-03-05 10:45:46 +0000

    exercise 01

---
+++ 01_c_basics/ex01/solution.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   solution.c                                             |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2026/03/05 10:36:12 by shobeedev               shobee4ever      */
+/*   Updated: 2026/03/05 10:43:51 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int main()
+{
+	float nbr;
+	printf("Enter a floating-point number : ");
+	scanf("%f",&nbr);
+
+	printf("\nThe number rounded to two decimal places is: %.2f",nbr);
+
+	return 0;
+}
