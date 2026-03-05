dbca2fc765aee3976c1f30f1202c82403a815d07
shobeedev <moha.saoud22@gmail.com>
2026-03-05 11:35:34 +0000

    exercise ex05

--- 01_c_basics/ex05/solution.c
+++ 01_c_basics/ex05/solution.c
@@ -5,34 +5,24 @@
 /*                                                            __) |   |_ \    */
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
-/*   Created: 2026/03/05 11:11:12 by shobeedev               shobee4ever      */
-/*   Updated: 2026/03/05 11:20:01 by shobeedev            tfaaty fi l3oolaa   */
+/*   Created: 2026/03/05 11:25:34 by shobeedev               shobee4ever      */
+/*   Updated: 2026/03/05 11:35:10 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-int tt_pow(int base,int exp)
-{
-	int i = 0;
-	int pow = 1;
-	while(i < exp)
-	{
-		pow = pow * base;
-		i++;
-	}
-	return pow;
-}
 
 int main()
 {
-	float raduis = 5;
-	const float PI = 3.14159;
+	float princibal,rate,time;
+	princibal = 1000;//for example you wnat to put many in a bank this is 1000 dollars.
+	rate = 8;//this rate of the princibal of your many.
+	time = 3;//and this for time example 3 years.
 
-	float areaOfCircle = PI * tt_pow(raduis,2);
-	float Circumference = 2 * PI * raduis;
+	float simpleInterset = (princibal * rate * time) / 100;
 
-	printf("Area of the circle is : %.3f\n",areaOfCircle);
-	printf("Circumference of the circle is : %.3f",Circumference);
+	printf("Simeple Interest is : %.2f",simpleInterset);
 
 	return 0;
 }
+
