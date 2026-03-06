470a1337470aab185fcaad82248d0b87d49c1cee
shobeedev <moha.saoud22@gmail.com>
2026-03-06 10:29:11 +0000

    exercise ex13

---
+++ 01_c_basics/ex13/solution.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   solution.c                                             |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2026/03/06 10:27:00 by shobeedev               shobee4ever      */
+/*   Updated: 2026/03/06 10:28:55 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("The first 10 natural number are : \n");
+
+	for(int i = 1;i <= 10;i++)
+		printf("%d ",i);
+
+	return 0;
+}
