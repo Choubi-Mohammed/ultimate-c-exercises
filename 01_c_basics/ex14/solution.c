d9652f6e641e07bb727f712767d123402fa79340
shobeedev <moha.saoud22@gmail.com>
2026-03-06 10:34:23 +0000

    exercise ex14

---
+++ 01_c_basics/ex14/solution.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   solution.c                                             |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2026/03/06 10:32:18 by shobeedev               shobee4ever      */
+/*   Updated: 2026/03/06 10:34:10 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Numbers in reserve order : \n");
+
+	int i = 10;
+	while(i >= 1)
+	{
+		printf("%d ",i);
+		i--;
+	}
+
+	return 0;
+}
