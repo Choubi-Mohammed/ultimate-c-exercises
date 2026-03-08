b3f41f8bc7a3888e7c009d67caadd11c8c95e62f
shobeedev <moha.saoud22@gmail.com>
2026-03-08 17:06:57 +0000

    exercise ex39

---
+++ 01_c_basics/ex39/solution.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   solution.c                                             |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2026/03/08 16:53:06 by shobeedev               shobee4ever      */
+/*   Updated: 2026/03/08 17:06:26 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+struct  stStudent
+{
+	char name[20];
+	int roll_nbr;
+	float marks;
+};
+
+int main()
+{
+	struct stStudent student[3];
+
+	int i = 0;
+	while(i < 3)
+	{
+		printf("--- Enter Data for Student %d ---\n",i+1);
+		printf("Name : ");
+		scanf("%s",&student[i].name);
+		printf("Roll number : ");
+		scanf("%d",&student[i].roll_nbr);
+		printf("Marks : ");
+		scanf("%f",&student[i].marks);
+		i++;
+	}
+
+	printf("\n--- All Student Records ---\n");
+	int j = 0;
+	while(j < 3)
+	{
+		printf("Name : %s\n",student[j].name);
+		printf("Roll number : %d\n",student[j].roll_nbr);
+		printf("Marks : %.2f%\n",student[j].marks);
+		j++;
+	}
+
+	return 0;
+}
