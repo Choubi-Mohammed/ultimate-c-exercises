c2a2d178301f0610b913cd6d67d0b56d132e5270
shobeedev <moha.saoud22@gmail.com>
2026-03-10 11:09:18 +0000

    exercise ex01

---
+++ 02_variables_data_types/ex01/solution.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   solution.c                                             |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2026/03/10 11:02:05 by shobeedev               shobee4ever      */
+/*   Updated: 2026/03/10 11:08:36 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int main()
+{
+	int a,b;
+
+	a = 20;
+	b = 10;
+
+	int sum = a + b;
+	int diff = a - b;
+	int prod = a * b;
+	float quot = a / b;
+	printf("%d + %d = %d\n",a,b,sum);
+	printf("%d - %d = %d\n",a,b,diff);
+	printf("%d * %d = %d\n",a,b,prod);
+	printf("%d / %d = %.2f\n",a,b,quot);
+
+	return 0;
+}
