c6735cd1a6749660e07248d1eb8fd6c76b9b0b11
shobeedev <moha.saoud22@gmail.com>
2026-03-10 21:41:08 +0000

    exercise ex16

---
+++ 02_variables_data_types/ex16/solution.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   solution.c                                             |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2026/03/10 21:37:17 by shobeedev               shobee4ever      */
+/*   Updated: 2026/03/10 21:40:56 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int main()
+{
+	int nbr;
+	printf("Enter a number : ");
+	scanf("%d",&nbr);
+
+	printf(",The character for ASCII %d is: %c\n",nbr,(char)nbr);
+
+	return 0;
+}
