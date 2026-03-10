f64028de1e5ecbc66d18f4c5f00ccb28e21bcb4a
shobeedev <moha.saoud22@gmail.com>
2026-03-10 21:32:39 +0000

    exercise ex14

---
+++ 02_variables_data_types/ex14/solution.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   solution.c                                             |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2026/03/10 21:27:13 by shobeedev               shobee4ever      */
+/*   Updated: 2026/03/10 21:32:26 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int main()
+{
+	int octal_num = 015;
+
+	printf("The octal number 015 in decimal is: %d\n",octal_num);
+
+	return 0;
+}
