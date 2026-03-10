c86c7aaefd81e46daefc92fd83afcac38541b1d6
shobeedev <moha.saoud22@gmail.com>
2026-03-10 11:40:20 +0000

    exercise ex05

---
+++ 02_variables_data_types/ex05/solution.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   solution.c                                             |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2026/03/10 11:35:46 by shobeedev               shobee4ever      */
+/*   Updated: 2026/03/10 11:39:11 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int main()
+{
+	int intVar = 42;
+	float floatVar = 13.37f;
+
+	printf("Convert int to float : %.2f\n",(float)intVar);
+	printf("Convert float to int : %d\n",(int)floatVar);
+
+	return 0;
+}
