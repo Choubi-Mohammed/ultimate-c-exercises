f437c93956d8b13309f49fff86bc3fe716acd092
shobeedev <moha.saoud22@gmail.com>
2026-03-11 21:28:30 +0000

    exercise ex04

--- 03_loops/ex04/solution.c
+++ 03_loops/ex04/solution.c
@@ -6,7 +6,7 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2026/03/11 11:34:36 by shobeedev               shobee4ever      */
-/*   Updated: 2026/03/11 11:41:14 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2026/03/11 11:42:19 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int main()
 		if(i % 2 != 0)
 			printf("%d ",i);
 		i++;
-	}while(i <= 200);
+	}while(i <= 20);
 
 	return 0;
 }
