704de53c28d178ba4f39fa85231646b5345278b0
shobeedev <moha.saoud22@gmail.com>
2026-03-14 12:37:57 +0000

    exercise ex07

--- 04_functions/ex07/solution.c
+++ 04_functions/ex07/solution.c
@@ -6,7 +6,7 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2026/03/14 12:32:38 by shobeedev               shobee4ever      */
-/*   Updated: 2026/03/14 12:35:44 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2026/03/14 12:37:50 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,8 @@ int find_max(int arr[], int size)
 int main()
 {
 	int numbers[] = {45, 12, 88, 5, 92, 33};
-
-	printf("The largest element in the array is: %d\n",find_max(numbers,6));
+	int size = sizeof(numbers) / sizeof(numbers[0]);
+	printf("The largest element in the array is: %d\n",find_max(numbers,size));
 
 	return 0;
 }
