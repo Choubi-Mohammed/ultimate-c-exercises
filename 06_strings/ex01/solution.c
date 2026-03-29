a698dc6ac75ae2b0778cbdea02f23e3b6caf44d8
shobeedev <moha.saoud22@gmail.com>
2026-03-29 07:09:03 +0100

    exercise ex01

---
+++ 06_strings/ex01/solution.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   solution.c                                             |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2026/03/29 06:44:50 by shobeedev               shobee4ever      */
+/*   Updated: 2026/03/29 07:07:41 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int main(void)
+{
+    char str[99];
+
+    printf("Enter s string (up to 99 characters): ");
+    scanf("%s",&str);
+
+    printf("You entered : %s\n",str);
+
+    return 0;
+}
