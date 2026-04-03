01a49a335b2b5be8118c52ff45516475df22a6a1
shobeedev <moha.saoud22@gmail.com>
2026-04-03 10:23:56 +0100

    exercise ex04

---
+++ 07_pointers/ex04/solution.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   solution.c                                             |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2026/04/03 07:46:13 by shobeedev               shobee4ever      */
+/*   Updated: 2026/04/03 10:23:39 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Size of int pointer (int *) : %d bytes\n",sizeof(int *));
+	printf("Size of float pointer (float *) : %d bytes\n",sizeof(float *));
+	printf("Size of char pointer (char *) : %d bytes\n\n",sizeof(char *));
+	printf("Size of int pointer (int) : %d bytes\n",sizeof(int));
+	printf("Size of float pointer (float) : %d bytes\n",sizeof(float));
+	printf("Size of char pointer (char) : %d bytes\n",sizeof(char));
+    return 0;	
+}
