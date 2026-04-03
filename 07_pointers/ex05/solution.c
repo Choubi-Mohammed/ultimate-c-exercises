a8193a910206a6a2e383f2193e4c722e5c5d9d78
shobeedev <moha.saoud22@gmail.com>
2026-04-03 10:41:15 +0100

    exercise ex05

--- 07_pointers/ex05/solution.c
+++ 07_pointers/ex05/solution.c
@@ -6,7 +6,7 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2026/04/03 10:26:11 by shobeedev               shobee4ever      */
-/*   Updated: 2026/04/03 10:37:00 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2026/04/03 10:41:01 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ int main()
 	printf("The adress stor in ptr = %p\n",ptr);
 	printf("The value in add of *ptr = %d\n\n",*ptr);
 	increment_value(ptr);
+	//or
+	//increment_value(&nbr);
 	printf("Operation (*ptr)++\n");
 	
 	printf("\n---- After function call ----\n");
