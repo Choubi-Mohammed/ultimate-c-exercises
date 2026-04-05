22c1f2af51d43b67f091cce514ab4cf5b459942b
shobeedev <moha.saoud22@gmail.com>
2026-04-05 11:40:43 +0100

    exercise ex32

--- 07_pointers/ex32/solution.c
+++ 07_pointers/ex32/solution.c
@@ -6,7 +6,7 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2026/04/05 11:12:24 by shobeedev               shobee4ever      */
-/*   Updated: 2026/04/05 11:24:26 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2026/04/05 11:40:34 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int main()
 	int arr[5] = {10, 20, 30, 40, 50};
 	int size = sizeof(arr) / sizeof(arr[0]);
 
-	int *ptr = arr;//or arr[0] or &arr[0]
+	int *ptr = arr;//or &arr[0]
 
 	int sum = 0;
 	float avg = 0;;
