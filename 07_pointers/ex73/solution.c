b335b51acc3c77d6149d2c8b19a1a5b4327f5b8d
shobeedev <moha.saoud22@gmail.com>
2026-04-08 11:19:56 +0100

    exercise ex73

--- 07_pointers/ex73/solution.c
+++ 07_pointers/ex73/solution.c
@@ -6,12 +6,12 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2026/04/08 10:28:39 by shobeedev               shobee4ever      */
-/*   Updated: 2026/04/08 10:49:46 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2026/04/08 11:19:41 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-void tt_strcpy(char *src,char *dest)
+char *tt_strcpy(char *src,char *dest)
 {
 	int i = 0;
 	while(src[i] != '\0')
@@ -20,17 +20,15 @@ void tt_strcpy(char *src,char *dest)
 		i++;
 	}
 	dest[i] = '\0';
+	return dest;
 }
-int main(int count,char *ar[])
+int main()
 {
-	if(count == 2)
-	{
-		char *src = ar[1];
+		char *src = "Hello world!";
 		char dest[20];
-		tt_strcpy(src,dest);
 		printf("Source : \"%s\"\n",src);
-		printf("Destination : \"%s\"\n",dest);
-	}
+		printf("Destination : \"%s\"\n",tt_strcpy(src,dest));
+
 
 	return 0;
 }
